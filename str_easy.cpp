7ee134904e8593a375433abd7df50e2d2f7b63ea
7London7 <106383366+7London7@users.noreply.github.com>
2023-10-09 12:37:49 +0300

    1

---
+++ str_easy.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+#include "streasy.h"
+using namespace std;
+
+string itc_hello_str(string name){
+    return "Hello, " + name;
+}
+long long itc_len(string str){
+    int i = 0;
+    int k = 0;
+    while(str[i] != '\0'){
+        k = k + 1;
+    i++;
+    }
+    return k;
+}
+void itc_print_copy_str(string str, int number){
+    while(number != 0){
+        cout << str;
+        number = number - 1;
+    }
+}
+void itc_first_end_three(string str){
+     int i = 0;
+    int k = 0;
+    while(str[i] != '\0'){
+        k = k + 1;
+    i++;
+    }
+    if(k > 5){
+            int i = 0;
+        while(str[i] != '\0'){
+            cout << str[0];
+    i++;
+
+        }
+    }
+    cout << str[0];
+    cout << str[1];
+    cout << str[3];
+    cout << str[4];
+
+}
---
+++ streasy.h
@@ -0,0 +1,12 @@
+#ifndef STREASY_H_INCLUDED
+#define STREASY_H_INCLUDED
+#include <iostream>
+#include <string>
+using namespace std;
+string itc_hello_str(string name);
+long long itc_len(string str);
+void itc_print_copy_str(string str, int number);
+void itc_first_end_three(string str);
+
+
+#endif // STREASY_H_INCLUDED
