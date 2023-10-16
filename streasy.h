5b4f5ed59e4fed74ff30f8cc98d2c2a120d78215
7London7 <106383366+7London7@users.noreply.github.com>
2023-10-16 12:44:48 +0300

    Update streasy.h

--- streasy.h
+++ streasy.h
@@ -7,7 +7,7 @@ string itc_hello_str(string name);
 long long itc_len(string str);
 void itc_print_copy_str(string str, int number);
 void itc_first_end_three(string str);
-
+int itc_count_char_in_str(char ch, string  str);
 
 #endif // STREASY_H_INCLUDED
 //https://www.geeksforgeeks.org/how-to-use-chatgpt-api-in-python/
